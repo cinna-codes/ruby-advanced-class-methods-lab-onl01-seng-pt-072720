b54ee3d994f5af7a85936748e9f216488fedc2ce
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-16 21:25:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -26,34 +26,35 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name) == false
-      self.create_by_name(song_name)
-    else
+    if self.find_by_name(song_name)
       self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
     end
-      return song_name.name
+  end
 
+  def self.alphabetical
+    @@all.sort_by { |s| s.name }
 
-    # if @@all.find { |song| song.name == song_name } == true
-    #   return song
-    # else
-    #   new_song = self.new
-    #   new_song.name = song_name
-    #   @@all << new_song
-    #   return new_song
-    # end
+  end
 
-    # @@all.each do |song|
-    #   if song.name == song_name
-    #     return song
-    #
-    #   else
-    #     new_song = self.new
-    #     new_song.name = song_name
-    #     return new_song
-    #   end
-    # end
+  def self.new_from_filename(song_name)
+    new_song = self.new
+    new_song.name = song_name[0...-4].split(" - ")[1]
+    new_song.artist_name = song_name[0...-4].split(" - ")[0]
+    new_song
+  end
 
+  def self.create_from_filename(song_name)
+    new_song = self.new
+    new_song.name = song_name[0...-4].split(" - ")[1]
+    new_song.artist_name = song_name[0...-4].split(" - ")[0]
+    @@all << new_song
+    new_song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def self.all
