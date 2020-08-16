d4ef17562506bce4deacb7c7e7ef2685450c1622
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-16 20:44:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,13 +39,13 @@ class Song
   end
 
   def self.new_from_filename(song_name)
-    new_song = self.new
+    song = self.new
     # data = song_name[0...-4].split(" - ")
     # song_name.name = data[0]
     # song_name.artist_name = data[1]
     # song_name
-    new_song.name = song_name[0...-4].split(" - ")[0]
-    new_song.artist_name = song_name[0...-4].split(" - ")[1]
+    song.name = song_name[0...-4].split(" - ")[0]
+    song.artist_name = song_name[0...-4].split(" - ")[1]
     #return new_song
     # data = song_name
     # song_name = self.new
